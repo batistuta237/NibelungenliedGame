992cf7e5717aed34081f6b449b30948d9fd9e907
batistuta237 <batistuta237@163.com>
2013-12-27 22:30:43 +0800

    Initial Commit

---
+++ NibelungenliedGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  NibelungenliedGameAppController.h
+//  NibelungenliedGame
+//
+//  Created by Wu Xiao dong on 13-12-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ NibelungenliedGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  NibelungenliedGameAppController.h
+//  NibelungenliedGame
+//
+//  Created by Wu Xiao dong on 13-12-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
